a968005be3859215c180d334cdca8e802326efbc
Vanessa Carraro <81984134+B10L0G4@users.noreply.github.com>
2023-11-06 16:26:18 -0300

    Delete exercicios directory

--- exercicios/js-curso-2-aula1/js-curso-2-aula_-/app.js
+++
@@ -1,9 +0,0 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Número Secreto o Jogo';
-
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1e 10';
-
-function verificarChute() {
-    console.log('Clique de botao')
-}
